37402ffe4fe4e89334c9809c1a69dde97c2af8e0
ummi-salamah <salamah258775@student.smkn1kandeman.sch.id>
2025-10-21 14:55:47 +0700

    Add files via upload

---
+++ Praktek _7.3_fungsi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktek__7._3_fungsi
+{
+    internal class Program
+    {
+        static int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka pertama: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int hasil = Tambah(x,y);
+            Console.WriteLine("Hasil penjumlahan: " + hasil);
+        }
+    }
+}
