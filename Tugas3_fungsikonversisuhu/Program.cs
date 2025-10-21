36bf1049e1a3cb0fb8e1e91e5c55e54c9923c600
ummi-salamah <salamah258775@student.smkn1kandeman.sch.id>
2025-10-21 15:01:32 +0700

    Add files via upload

---
+++ Tugas3_fungsikonversisuhu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas3_fungsikonversisuhu
+{
+    internal class Program
+    {
+        static double KonversiSuhu(double C)
+        {
+            double F = (C * 9 / 5) + 32;
+            return F;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===Program Konversi Suhu Celcius ke Fahrenheit===");
+
+            //Meminta Input dari Pengguna
+            Console.Write("Masukkan suhu dalam Celcius: ");
+            double suhuC = double.Parse(Console.ReadLine());
+
+            //Memanggil fungsi KonversiSuhu dan menampilkan hasilnya
+            double suhuF = KonversiSuhu(suhuC);
+            Console.Write("Suhu Dalam Fahrenhait: " + suhuF);
+        }
+    }
+}
---
+++ Tugas3_fungsikonversisuhu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tugas3_fungsikonversisuhu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tugas3_fungsikonversisuhu")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d6a9432c-29e2-4acc-8afc-e9205db5ae39")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
