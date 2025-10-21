30374415bdedd07eedb55683ca83bad18808b221
ummi-salamah <salamah258775@student.smkn1kandeman.sch.id>
2025-10-21 14:54:03 +0700

    Add files via upload

---
+++ Praktek7.1_Ummi Salamah_X PPLG 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktek7._1_Ummi_Salamah_X_PPLG_2
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            Sapa(); // Memanggil fungsi
+        }
+    }
+}
