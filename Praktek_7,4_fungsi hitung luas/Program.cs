41e9de6d1e18358c777d7c29b163d1c578cec0a1
ummi-salamah <salamah258775@student.smkn1kandeman.sch.id>
2025-10-21 14:59:01 +0700

    Add files via upload

---
+++ Praktek_7,4_fungsi hitung luas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktek_7_4_fungsi_hitung_luas
+{
+    internal class Program
+    {
+        static double HitungLuas(double panjang, double lebar)
+        {
+            double luas = panjang * lebar;
+            return luas;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan panjang: ");
+            double p = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan lebar: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = HitungLuas(p, l);
+            Console.WriteLine("Luas persegi panjang adalah: " + hasil);
+        }
+    }
+}
---
+++ Praktek_7,4_fungsi hitung luas/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Praktek_7,4_fungsi hitung luas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praktek_7,4_fungsi hitung luas")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0071293e-4ffd-4c76-83b5-7d335b588721")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
