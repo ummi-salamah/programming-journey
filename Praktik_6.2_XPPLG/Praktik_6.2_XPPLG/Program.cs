4937760ae7be69f0fa9df1a17e6107f471b67d82
ummi-salamah <salamah258775@student.smkn1kandeman.sch.id>
2025-11-19 07:47:33 +0700

    Add files via upload

---
+++ Praktik_6.2_XPPLG/Praktik_6.2_XPPLG/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_6._2_XPPLG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variabel untuk menyimpan hasil penjumlahan
+            int total = 0;
+
+            Console.WriteLine("Menghitung jumlah angka dari 1 sampai 5:");
+
+            //Perulangan for dari 1 hingga 5
+            for (int i = 1; i <= 5; i++)
+            {
+                // Tambahkan nilai i saat ini ke variabel total
+                total = total + i; // Bisa juga ditulis sebagai: total += i:
+
+                //Tampilkan proses penjumlahan di setiap langkah 
+                Console.WriteLine("Menambahkan " + i + ", total sementara = " + total);
+            }
+                //Tampilkan hasil akhir setelah perulangan selesai 
+                Console.WriteLine("\nHasil akhir penjumlahan adalah " + total);
+        }
+    }
+}
---
+++ praktik_6.1_XPPLG2/praktik_6.1_XPPLG2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("praktik_6.1_XPPLG2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("praktik_6.1_XPPLG2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b456d2fc-36ef-4a6f-b848-96a68da51faa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
